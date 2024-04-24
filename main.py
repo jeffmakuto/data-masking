4ee1338207bd4d56e0a040ac099f904f49614c74
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-04-24 17:18:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,10 @@ def main() -> None:
     """
     creating the dataframe from loaded csv
     """
-    csv_data = """
-    ID,Name,Age,Phone,Email,
-    Card Number
-    1,Martin tapelia,33,(254) 743-376-987,marttapelia@gmail.com,
-    1245-5698-6325-0178
-    2,Bella Kafukuswi,27,(254) 734-987-097,bellakafukuswi@gmail.com,
-    8796-0987-5432-9634
-    3,Benjamin Msumari,54,(254) 789-085-321,benjamsumari@gmail.com,
-    8906-8742-9651-1287
-    4,Stano Kimoda,42,(254) 712-643-289,stanokimoda@gmail.com,
-    7143-0245-7245-0156
-    5,Collo Musambati,19,(254) 789-765-014,collomusambati@gmail.com,
-    0145-7613-0267-3891
-    """
+    # Read the content of 'data.txt' and assign it to the variable 'csv_data'
+    with open('data.txt', 'r') as file:
+        csv_data = file.read()
+
      output_file = "output.csv"
      process_data(csv_data, output_file)
 
