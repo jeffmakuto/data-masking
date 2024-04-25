7e9ebd0c30c630eacc33f3c1815793abe777c47c
jeffmakuto <hjeffmakuto@gmail.com>
2024-04-25 08:49:55 +0300

    mask the email addresses

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 """ Main script """
 from load_csv import DataFrameLoader
 from process_data import process_data
+from masked import MaskedDataHandler
+from data_mask import DataMasker
 
 
 def main() -> None:
@@ -12,8 +14,22 @@ def main() -> None:
     with open('data.txt', 'r') as file:
         csv_data = file.read()
 
+    # Load CSV data and process it
     output_file = "output.csv"
     process_data(csv_data, output_file)
+
+    # Load CSV data again to mask email addresses
+    loader = DataFrameLoader()
+    loader.load_csv(csv_data)
+
+    # Mask email addresses
+    data_mask = DataMasker(loader)
+    data_mask.mask_email_address()
+
+    # Save the masked data to a new file
+    masked_output_file = "masked.csv"
+    masked_data = MaskedDataHandler(loader)
+    masked_data.save_masked_data(masked_output_file)
 
 
 if __name__ == "__main__":
--- masked.py
+++ masked.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 """ Masked data handling """
-from data_mask import DataMasker
+from load_csv import DataFrameLoader
 
 
 class MaskedDataHandler:
     """
     Handles saving masked data
     """
+    def __init__(self, loader: DataFrameLoader) -> None:
+        """
+        Initializes a MaskedDataHandler object with a DataFrameLoader
+        instance
+
+        Args:
+            loader(DataFrameLoader): an instance of
+            DataFrameLoader
+        """
+        self.loader = loader
+
     def save_masked_data(self, output_file: str) -> None:
         """
         Saves masked data frame to a CSV file
@@ -16,4 +27,3 @@ class MaskedDataHandler:
         """
         self.loader.df.to_csv(output_file, index=False)
         print(f"Masked data has been saved to {output_file}")
-
