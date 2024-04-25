f223d17cb477da7a55766bd72da0b0ba6d6bebc6
jeffmakuto <hjeffmakuto@gmail.com>
2024-04-25 09:35:14 +0300

    Modularize the code further

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
 #!/usr/bin/env python3
 """ Main script """
-from load_csv import DataFrameLoader
-from process_data import process_data
-from masked import MaskedDataHandler
-from data_mask import DataMasker
+from process import DataProcessor
 
 
-def main() -> None:
-    """
-    creating the dataframe from loaded csv
-    """
-    # Read the content of 'data.txt' and assign it to the variable 'csv_data'
-    with open('data.txt', 'r') as file:
-        csv_data = file.read()
+def main():
+    input_file = 'data.txt'
+    output_file = 'output.csv'
+    masked_output_file = 'masked.csv'
 
-    # Load CSV data and process it
-    output_file = "output.csv"
-    process_data(csv_data, output_file)
-
-    # Load CSV data again to mask email addresses
-    loader = DataFrameLoader()
-    loader.load_csv(csv_data)
-
-    # Mask email addresses
-    data_mask = DataMasker(loader)
-    data_mask.mask_email_address()
-
-    # Save the masked data to a new file
-    masked_output_file = "masked.csv"
-    masked_data = MaskedDataHandler(loader)
-    masked_data.save_masked_data(masked_output_file)
+    processor = DataProcessor(input_file, output_file, masked_output_file)
+    processor.process()
 
 
 if __name__ == "__main__":
---
+++ process.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+""" data processing """
+from load_csv import DataFrameLoader
+from process_data import process_data
+from data_mask import DataMasker
+from masked import MaskedDataHandler
+
+
+class DataProcessor:
+    """
+    class to process the data
+    """
+    def __init__(self, input_file: str, output_file: str, masked_output_file: str) -> None:
+        """
+        Initialization
+
+        Args:
+            input_file (str): original file containing data
+            output_file (str): dataframed data
+            masked_output_file (str): masked data
+        """
+        self.input_file = input_file
+        self.output_file = output_file
+        self.masked_output_file = masked_output_file
+
+    def process(self) -> None:
+        """
+        method to process data
+        """
+        loader = DataFrameLoader()
+        with open(self.input_file, 'r') as file:
+            csv_data = file.read()
+
+        # Load CSV data and process it
+        process_data(csv_data, self.output_file)
+
+        # Load CSV data again to mask email addresses
+        loader.load_csv(csv_data)
+
+        # Mask email addresses
+        data_masker = DataMasker(loader)
+        data_masker.mask_email_address()
+
+        # Save the masked data to a new file
+        masked_data = MaskedDataHandler(loader)
+        masked_data.save_masked_data(self.masked_output_file)
